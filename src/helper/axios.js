79d2f4c31b3cd48da054ab1f5c49ed4f68bc8859
harshsakhiya97 <107310577+harshsakhiya97@users.noreply.github.com>
2024-04-21 17:56:36 +0530

    changed base path

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8080/api/v1/app/",
-  baseURL: "http://3.111.138.176:8080/PrideServer/api/v1/app/",
+  // baseURL: "http://3.111.138.176:8080/PrideServer/api/v1/app/",
+  baseURL: "https://api.pridecomputer.com:8443/PrideServer/api/v1/app/",
   headers: {
     "Content-Type": "application/json",
   },
